4d7ca28a9499ce76b3ac8bb6957fb5372d87f99c
Hoang Phong <hoangphong7889@gmail.com>
2025-07-28 09:30:29 +0700

    static files

--- src/sever.js
+++ src/sever.js
@@ -1,26 +1,32 @@
 const express = require('express') // commomjs
 //import express from 'express'; // es modules
 // const path = require('path') //
-const app = express() // app express
-const port = 8080 // port
+require('dotenv').config()
 
+//console.log(process.env)
+
+const app = express() // app express
+const port = process.env.PORT || 8888// port => hardcode
+const hostname = process.env.HOST_NAME
 //config template engine
 app.set('views', './src/views')
 app.set('view engine', 'ejs')
+//config static files 
+app.use(express.static('./src'))
 
 // khai bao route
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//     res.send('Hello World! PUNNO')
+// })
 
 app.get('/abc', (req, res) => {
     res.send('Check abc')
 })
-app.get('/punno', (req, res) => {
+app.get('/', (req, res) => {
     //res.send('<h1>ABC</h1>')
     res.render('sample.ejs')
 })
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`)
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`)
 })
