bd26455676aabb1eb2af28fa370cc0bd3f37751d
Hoang Phong <hoangphong7889@gmail.com>
2025-07-30 12:28:47 +0700

    SQL

--- src/sever.js
+++ src/sever.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
 
-
+const mysql = require('mysql2')
 
 const app = express() // app express
 const port = process.env.PORT || 8888// port => hardcode
@@ -23,8 +23,23 @@ app.use(express.static(path.join(__dirname, 'public')))
 // khai bao route
 app.use('/', webRoutes)
 
+// test connection database
+const connection = mysql.createConnection({
+    host: 'localhost', //default: 3306
+    port: 3307, // default: empty
+    user: 'root',
+    password: '123456',
+    database: 'hoidanit'
+});
 
+connection.query(
+    'select * from Users u ',
 
+    function (err, results, fields) {
+        console.log(">>>results--", results); // results contains rows returned by server
+        console.log(">>>fields--", fields); // fields contains extra meta data about results, if available
+    }
+);
 
 
 
