e9f5223022ecd70abda34678b617bbbde6d15e31
Hoang Phong <hoangphong7889@gmail.com>
2025-07-30 14:15:27 +0700

    database

--- src/sever.js
+++ src/sever.js
@@ -6,8 +6,8 @@ require('dotenv').config()
 //console.log(process.env)
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
+const connection = require('./config/database')
 
-const mysql = require('mysql2')
 
 const app = express() // app express
 const port = process.env.PORT || 8888// port => hardcode
@@ -23,14 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 // khai bao route
 app.use('/', webRoutes)
 
-// test connection database
-const connection = mysql.createConnection({
-    host: 'localhost', //default: 3306
-    port: 3307, // default: empty
-    user: 'root',
-    password: '123456',
-    database: 'hoidanit'
-});
+// test connection 
+
 
 connection.query(
     'select * from Users u ',
